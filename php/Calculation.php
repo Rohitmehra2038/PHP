3a75cdf325ebb78714ca56defbb098d48409da87
Rohit Mehra <142257980+Rohitmehra2038@users.noreply.github.com>
2024-09-02 10:39:50 +0530

    Add files via upload

---
+++ php/Calculation.php
@@ -0,0 +1,26 @@
+<?php
+
+$x=10;
+$y=15;
+
+$a=$x + $y;
+
+$b=$x - $y;
+
+$c=$x * $y;
+
+$d=$x / $y;
+
+$e=$x % $y;
+
+echo "The solution of the x+y is:".$a;
+echo "<br>";
+echo "The solution of the x-y is:".$b;
+echo "<br>";
+echo "The solution of the x*y is:".$c;
+echo "<br>";
+echo "The solution of the x/y is:".$d;
+echo "<br>";
+echo "The solution of the x%y is:".$e;
+echo "<br>";s
+?>
